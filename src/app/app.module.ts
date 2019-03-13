af53b514c07b16dd92e02d8fe84f78111a621738
RenatoDiPierno <renatodipi@gmail.com>
2019-03-13 10:46:10 +0100

    Creation all component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { GameListComponent } from './game-list/game-list.component';
+import { GameDetailComponent } from './game-detail/game-detail.component';
+import { EditGameComponent } from './edit-game/edit-game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HomeComponent,
+    GameListComponent,
+    GameDetailComponent,
+    EditGameComponent
   ],
   imports: [
     BrowserModule
